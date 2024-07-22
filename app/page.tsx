2c5d45175713124aeeeb92c6be67ff372be5ebce
Mateus Santos <m.santos.azeve@gmail.com>
2024-07-22 19:02:47 -0300

    feat: adiciona o metadata da pagina inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-import Link from 'next/link'
- 
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "IEC • Página Inicial",
+};
+
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
       <Link href="/about">About</Link>
     </div>
-  )
+  );
 }
