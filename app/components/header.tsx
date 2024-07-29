0a34af7e5f09713c68ff5f2d344d43b41560da32
Mateus Santos <m.santos.azeve@gmail.com>
2024-07-29 17:38:04 -0300

    feat: adiciona root componentes

---
+++ app/components/header.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./navbar";
+
+export default function Header() {
+  return (
+    <header>
+      {/* Insere logotipo aqui */}
+      <Navbar />
+    </header>
+  );
+}
---
+++ app/components/navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav>
+      {/* Cria o botao collapse para tela menores aqui */}
+      <div>
+        <ul>
+          <li>Cultos</li>
+          <li>Corpo ministerial</li>
+          <li>Quem somos</li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
